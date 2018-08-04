c8ab8e40f5d7863df3f59d0f770191c21eb70a03
Alexander Auer <mail.auer.alex@icloud.com>
2018-08-04 18:39:38 +0200

    Updates for autostart

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
+#!/usr/bin/env python3
 import logging
 import json
 import time
 import process 
 
 ## create logger
-logging.basicConfig(format="%(asctimes)s:%(levelname)s:%(message)s", filename="/logs/sensor.log", level=logging.ERROR)
+logging.basicConfig(format="%(asctimes)s:%(levelname)s:%(message)s", filename="./SenseHat/logs/sensor.log", level=logging.ERROR)
 
 ## load config data form config.json
-with open('config.json') as config_file:
+with open('./SenseHat/config.json') as config_file:
     config = json.load(config_file)
 
-print("Client started.")
+print("Miblab weather client started. Don't close this window.")
 
+## start main task
 while True:
 
     if process.check_connectivity(config["connectionModul"]) == False:
@@ -22,12 +24,12 @@ while True:
     tries = 0
 
     while tries < 3:
-
+        tries += 1
+        
         try:
             response = process.send_message(config,payload)
 
             if(response["command"]["resend"]):
-                tries += 1
                 logging.error("Server placed a resend. Response: " + response)
             else:
                 break
